830ca654539588ba6de9a8825f542aa50d6d2b1f
Marek <wojnarmarek0@gmail.com>
2022-05-15 22:01:09 +0200

    up

---
+++ src/main/java/com/example/demo/Recipe.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+        import javax.persistence.Entity;
+        import javax.persistence.GeneratedValue;
+        import javax.persistence.GenerationType;
+        import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String type;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
