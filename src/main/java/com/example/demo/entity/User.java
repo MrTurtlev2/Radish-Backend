094c3a50f33c5ab71a3c6e20d88364c5505f40de
Marek <wojnarmarek0@gmail.com>
2023-02-27 20:45:34 +0100

    one to many table relations

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -6,13 +6,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Plant {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,9 +21,7 @@ public class Plant {
     @Column
     private String name;
     @Column
-    private String description;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    private String lastName;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Plant> list;
 }
