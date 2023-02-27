244978294f8b0592c6cd5ca647fde383defd5a39
Marek <wojnarmarek0@gmail.com>
2023-02-27 21:11:34 +0100

    seeding data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO User (id, name, last_name) VALUES
+    (1,'Marek', 'Wojnar'),
+    (2,'Michał', 'Dolas');
+
+
+INSERT INTO Plant (id, description, name, user_id) VALUES
+    (1,'suchy', 'kaktus', 1),
+    (2,'suchy2', 'kaktus2', 2);
